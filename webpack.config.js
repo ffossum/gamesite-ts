8489d9db2e8b21548bacdac6b13d7f5a9d6f0699
Fredrik Fossum <fredrik.fossum@gmail.com>
2018-05-05 13:55:30 +0200

    prettier config

--- webpack.config.js
+++ webpack.config.js
@@ -4,19 +4,19 @@ module.exports = {
   entry: "./src/client/index",
   output: {
     path: path.resolve(__dirname, "dist"),
-    filename: "bundle.js"
+    filename: "bundle.js",
   },
   mode: "development",
   resolve: {
-    extensions: [".ts", ".tsx", ".js", ".jsx", ".json"]
+    extensions: [".ts", ".tsx", ".js", ".jsx", ".json"],
   },
   module: {
     rules: [
       {
         test: /\.(tsx?)|(jsx?)$/,
         exclude: /node_modules/,
-        loader: "babel-loader"
-      }
-    ]
-  }
+        loader: "babel-loader",
+      },
+    ],
+  },
 };
