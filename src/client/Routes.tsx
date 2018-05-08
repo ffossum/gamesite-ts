001358bb2d17fda55ee8c3781b6d0ed4dfcace20
Fredrik Fossum <fredrik.fossum@gmail.com>
2018-05-08 20:57:03 +0200

    Add GameRoomContainer

--- src/client/Routes.tsx
+++ src/client/Routes.tsx
@@ -3,7 +3,7 @@ import { Route, Switch } from "react-router-dom";
 import FrontPage from "./components/FrontPage";
 import NotFound from "./components/NotFound";
 import Settings from "./components/settings/Settings";
-// import GameRoomContainer from "./redux/modules/games/GameRoomContainer";
+import GameRoomContainer from "./redux/modules/games/GameRoomContainer";
 // import LobbyContainer from "./redux/modules/lobby/LobbyContainer";
 import LoginFormContainer from "./redux/modules/login/LoginFormContainer";
 import NavContainer from "./redux/modules/nav/NavContainer";
@@ -15,8 +15,8 @@ export default function Routes() {
       <NavContainer />
       <Switch>
         <Route exact path="/" component={FrontPage} />
-        {/* <Route path="/lobby" component={LobbyContainer} />
-        <Route path="/game/:gameId" component={GameRoomContainer} /> */}
+        {/* <Route path="/lobby" component={LobbyContainer} /> */}
+        <Route path="/game/:gameId" component={GameRoomContainer} />
         <Route exact path="/registration" component={RegistrationFormContainer} />
         <Route exact path="/login" component={LoginFormContainer} />
         <Route path="/settings" component={Settings} />
---
+++ src/client/redux/modules/games/GameRoomContainer.ts
@@ -0,0 +1,99 @@
+import { contains } from "ramda";
+import { connect, Dispatch } from "react-redux";
+import { match } from "react-router-dom";
+import { bindActionCreators } from "redux";
+import { UserId } from "../../../../common/user";
+import GameRoom from "../../../components/game/GameRoom";
+import { Props as GameRoomProps } from "../../../components/game/GameRoom";
+import { State } from "../root";
+import {
+  cancelGame,
+  enterRoom,
+  enterSpectatorRoom,
+  exitRoom,
+  exitSpectatorRoom,
+  joinGame,
+  leaveGame,
+  startGame,
+} from "./gameRoomActions";
+
+interface OwnProps {
+  location: Location;
+  history: History;
+  match: match<{ gameId?: string }>;
+}
+
+export function mapStateToProps(state: State, ownProps: OwnProps) {
+  const gameId = ownProps.match.params.gameId || "";
+  const game = state.games[gameId];
+  return {
+    gameId,
+    game,
+    users: state.users,
+    user: state.session.user,
+  };
+}
+
+export function mapDispatchToProps(dispatch: Dispatch<State>) {
+  return bindActionCreators(
+    {
+      enterRoom,
+      exitRoom,
+      enterSpectatorRoom,
+      exitSpectatorRoom,
+      joinGame,
+      leaveGame,
+      cancelGame,
+      startGame,
+    },
+    dispatch
+  );
+}
+
+export function mergeProps(
+  stateProps: ReturnType<typeof mapStateToProps>,
+  dispatchProps: ReturnType<typeof mapDispatchToProps>
+  // ownProps,
+): GameRoomProps {
+  const { game, gameId, users, user } = stateProps;
+
+  const placeholderUser = (userId: UserId) => ({ id: userId, username: "" });
+
+  const transformedGame = game && {
+    id: gameId,
+    host: users[game.host] || placeholderUser(game.host),
+    players: game.players.map(playerId => users[playerId] || placeholderUser(playerId)),
+    status: game.status,
+  };
+
+  const isInGame = !!(game && user && contains(user.id, game.players));
+
+  return {
+    enterRoom(enteredGameId) {
+      const shouldFetchGameData = !isInGame;
+      dispatchProps.enterRoom(enteredGameId, shouldFetchGameData);
+    },
+    exitRoom(exitedGameId) {
+      dispatchProps.exitRoom(exitedGameId, isInGame);
+    },
+    enterSpectatorRoom: dispatchProps.enterSpectatorRoom,
+    exitSpectatorRoom: dispatchProps.exitSpectatorRoom,
+    joinGame() {
+      user && dispatchProps.joinGame(user.id, gameId);
+    },
+    leaveGame() {
+      user && dispatchProps.leaveGame(user.id, gameId);
+    },
+    cancelGame() {
+      user && game && user.id === game.host && dispatchProps.cancelGame(user.id, game.id);
+    },
+    startGame() {
+      user && game && user.id === game.host && dispatchProps.startGame(user.id, game.id);
+    },
+    gameId,
+    game: transformedGame,
+    user,
+  };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps, mergeProps)(GameRoom);
